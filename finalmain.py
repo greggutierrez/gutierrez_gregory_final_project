34958746f59551736a424d2088d5c0f592479612
greggutierrez <142935167+greggutierrez@users.noreply.github.com>
2023-12-19 22:30:37 -0800

    Delete finalmain.py

--- finalmain.py
+++
@@ -1,9 +0,0 @@
-# This file was created by: Gregory Gutierrez
-# Project Title: Video Game Design
-
-# Project Goals:
-# Create smooth running game with minimal errors
-# Make the game enjoyable
-# Have a Start and end scren
-# Make the game creative and unique
-# Make it a type of game that can have future updates even after the semester
